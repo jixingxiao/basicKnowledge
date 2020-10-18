95e8ae881a1825c72921779b7982f16ebd6a028d
jixingxiao <976013085@qq.com>
2020-10-18 22:44:56 +0800

    add list progect

---
+++ algorithm/list/listsolution.h
@@ -0,0 +1,39 @@
+﻿#ifndef LISTSOLUTION_H
+#define LISTSOLUTION_H
+#include <list>
+
+using namespace std;
+
+void removeDuplicatesFromSortedList(list<int>& srcList)
+{
+    if ( srcList.empty() ) return;
+    list<int>::iterator itfirst = srcList.begin();
+    list<int>::iterator itsecond  = itfirst++;
+    while( itfirst != srcList.end() )
+    {
+        if ( *itfirst == *itsecond )
+        {
+            itfirst = srcList.erase(itfirst);
+        }
+        else
+        {
+            itsecond = itfirst;
+            itfirst++;
+        }
+    }
+
+}
+
+void addList(list<int>& list1, list<int>& list2)
+{
+    if ( list1.empty() || list2.empty() )
+    {
+        for(list<int>::iterator iter = list2.begin(); iter != list2.end(); ++iter)
+        {
+            list1.push_back(*iter);
+        }
+        return;
+    }
+
+}
+#endif // LISTSOLUTION_H
---
+++ algorithm/list/main.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include "listsolution.h"
+
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    list<int> list1;
+    list1.push_front(2);
+    list1.push_front(5);
+    list1.push_front(6);
+    list1.push_front(6);
+    list1.push_front(6);
+    list1.push_front(8);
+    list1.push_front(10);
+    list1.push_front(12);
+    list1.push_front(12);
+
+    for(list<int>::iterator i = list1.begin(); i != list1.end(); ++i)
+    {
+        cout << *i << " ";
+    }
+
+    cout << "print finish >>>>>>>>>>>>>" << endl;
+    removeDuplicatesFromSortedList(list1);
+
+    for(list<int>::iterator i = list1.begin(); i != list1.end(); ++i)
+    {
+        cout << *i << " ";
+    }
+    return 0;
+}
